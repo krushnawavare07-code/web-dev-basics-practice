d65c1fdcf85257bfaaae952fcc45f39d85496b4a
Krushna Wavare <krushnawavare07@gmail.com>
2026-03-05 17:17:55 +0530

    es6

---
+++ es6.js
@@ -0,0 +1,18 @@
+//normal function
+function add(x,y){
+    return x+y;
+}
+
+let result =add(5,10);
+console.log(result);
+
+//arrow function
+let add2 =(x,y)=> x+y;
+
+let result2=add2(12,34);
+console.log(result2);
+
+let name ="krushna";
+console.log(`hello ${name}`)
+
+
